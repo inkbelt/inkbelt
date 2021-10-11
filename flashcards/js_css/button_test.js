c10eedbea92c5fc04a48cc38c1327991aea2dc65
Eagle Dog <ethandragoin@gmail.com>
2021-10-11 05:45:44 -0600

    add button functions

--- flashcards/js_css/button_test.js
+++ flashcards/js_css/button_test.js
@@ -2,81 +2,75 @@
 //    button_test.js
 //
 
-let plus = true;
-let minus = false;
-let multiply = false;
-let divide = false;
-
-let plus_style = document.getElementById('plus').style
-let minus_style = document.getElementById('minus').style
-let multiply_style = document.getElementById('multiply').style
-let divide_style = document.getElementById('divide').style
 
 
-function Math_Go(sign, style) {
-  console.log(sign);
-  if (sign == false) {
-    sign = true;
-    style.background = '#eee';
+window.addEventListener("load", FixButtonColors)
+
+
+const color_on = '#eee';
+const color_off = '#aaa';
+
+
+
+let plus = { active: true,
+             operand: '+',
+             style: document.getElementById('plus').style }
+let minus = { active: false, 
+              operand: '-',
+              style: document.getElementById('minus').style }
+let multiply = { active: false, 
+              operand: 'x',
+              style: document.getElementById('multiply').style }
+let divide = { active: false, 
+              operand: '&divide;',
+              style: document.getElementById('divide').style }
+
+let operand_objects = [plus, minus, multiply, divide];
+
+function FixColor(symbol) {
+  if (symbol.active == true) {
+    symbol.style.background = color_on
+  } else {
+    symbol.style.background = color_off
+  }
+}
+
+function FixButtonColors() {
+  FixColor(plus);
+  FixColor(minus);
+  FixColor(multiply);
+  FixColor(divide);
+}
+
+
+
+function Math_Go(symbol) {
+  if (symbol.active == true) {
+    symbol.active = false;
+    symbol.style.background = color_off;
   }
   else {
-    sign = false;
-    style.background = '#aaa';
+    symbol.active = true;
+    symbol.style.background = color_on;
   }
+  console.log(symbol);
+  UpdateOperands();
 }
 
+function Plus_Go() { Math_Go(plus); }
+function Minus_Go() { Math_Go(minus); }
+function Multiply_Go() { Math_Go(multiply); }
+function Divide_Go() { Math_Go(divide); }
 
-function Plus_Go2() {
-  Math_Go(plus, plus_style);
+
+function UpdateOperands() {
+  operands = [];
+  operand_objects.forEach(function(symbol){
+    if (symbol.active == true) {
+      operands.push(symbol.operand);
+    }
+
+  })
 }
 
-
-function Plus_Go() {
-  console.log(plus)
-  if (plus == false) {
-    plus = true;
-    plus_style.background = '#eee';
-  } 
-  else {
-    plus = false;
-    plus_style.background = '#aaa';
-  }
-}
-
-
-
-function Minus_Go() {
-  if (minus == true) {
-    minus = false;
-    minus_style.background = '#eee';
-  } else {
-    minus = true;
-    minus_style.background = '#aaa';
-  }
-  console.log(minus)
-}
-
-
-function Mult_Go() {
-  if (multiply == true) {
-    multiply = false;
-    multiply_style.background = '#eee';
-  } else {
-    multiply = true;
-    multiply_style.background = '#aaa';
-  }
-  console.log(multiply)
-}
-
-
-function Div_Go() {
-  if (divide == true) {
-    divide = false;
-    divide_style.background = '#eee';
-  } else {
-    divide = true;
-    divide_style.background = '#aaa';
-  }
-  console.log(divide)
-}
 
---
+++ flashcards/js_css/flashcards.js
@@ -0,0 +1,120 @@
+//
+//    flashcards.js
+//
+
+
+
+let num1;
+let num2;
+let num3;
+let num4;
+let feedback = '';
+let operand;
+let operands = [];
+let answer;
+let response;
+let thinking = 0;
+
+let level = 'hard';  // 'easy' 'medium' 'hard'
+
+operands = ['+', '-', 'x', '&divide;'];
+
+
+NewCard();
+//	FocusCursor();
+
+function NewCard() {
+	console.log('New Card.');
+	num1 = Math.floor(Math.random() * 9 + 4);
+	num2 = Math.floor(Math.random() * 6 + 3);
+	ChooseOperand();
+	NoNegatives();
+	GetAnswer();
+	response = 0;
+	document.getElementById('num1').innerHTML = num1;
+	document.getElementById('operand').innerHTML = operand;
+	document.getElementById('num2').innerHTML = num2;
+	UpdateResponse();
+	setTimeout(function() { ClearFeedback(); }, 700);
+}
+
+
+//const randomElement = array[Math.floor(Math.random() * array.length)];
+
+
+
+function ChooseOperand() {
+	operand = operands[Math.floor(Math.random() * operands.length)];
+	console.log(operand);
+}
+
+function NoNegatives() {
+	if (operand == '-' && num2 > num1) {
+		num3 = num1; num1 = num2; num2 = num3; // swap numbers
+	}
+}
+
+function DivideIt() {
+	num3 = num1 * num2;
+	answer = num1;
+	num1 = num3;
+	//return num4;
+}
+
+function GetAnswer() {
+	if (operand == '+') {	answer = num1 + num2; }
+	if (operand == '-') {	answer = num1 - num2; }
+	if (operand == 'x') {	answer = num1 * num2; }
+	if (operand == '&divide;') { DivideIt(); }
+}
+
+function ClearFeedback() {
+	feedback = '';
+	document.getElementById('feedback').innerHTML = feedback;
+	console.log('Feedback cleared.');
+}
+
+function UpdateFeedback() {
+	document.getElementById('feedback').innerHTML = feedback;
+	console.log('Feedback updated.');
+}
+
+function UpdateResponse() {
+	setTimeout(function() {
+		thinking = 0;
+		FocusCursor();
+		document.getElementById('response').value = '';
+		console.log('Response updated.');
+	}, 200);
+}
+
+function FocusCursor() {
+	document.getElementById('response').focus();
+}
+
+document.onkeydown = function() {	 // Enter key submits
+  if (window.event.keyCode == '13') {
+  	if (thinking != 1) { Submit(); }
+  }
+}
+
+function Submit() {
+	thinking = 1;
+	FocusCursor();
+	response = document.getElementById('response').value;
+	console.log('Submitted. Response:  '+response+'.  Answer:  '+answer+'.');
+	if (response == answer) {
+		feedback = 'Correct!';
+		console.log('       CORRECT');
+		UpdateFeedback();
+		NewCard();
+	} else {
+		feedback = 'Try again.';
+		console.log('       INCORRECT');
+		UpdateFeedback();
+		UpdateResponse();
+		setTimeout(function() {
+			ClearFeedback();
+		}, 700);
+	}
+}
